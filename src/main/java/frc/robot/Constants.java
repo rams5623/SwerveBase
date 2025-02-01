173a5d4512ecdeca5b8d397175bfdcd3019f58c2
rams5623 <roboticramsfrcteam5623@gmail.com>
2025-02-01 16:04:50 -0500

    speed change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,10 +19,10 @@ import swervelib.math.Matter;
 public final class Constants
 {
 
-  public static final double ROBOT_MASS = (80.0) * 0.453592; // 32lbs * kg per pound
+  public static final double ROBOT_MASS = (40) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(5);
+  public static final double MAX_SPEED  = Units.feetToMeters(8.2);
   // Maximum speed of the robot in meters per second, used to limit acceleration.
 
 //  public static final class AutonConstants
